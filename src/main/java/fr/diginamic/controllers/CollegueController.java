bcd7dbec61e0a329bc13a32621a86dcbc8028e69
Olivier LASBLEIS <lasbleis.olivier@yahoo.fr>
2019-08-28 17:06:26 +0200

    deuxieme

--- src/main/java/fr/diginamic/controllers/CollegueController.java
+++ src/main/java/fr/diginamic/controllers/CollegueController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +11,6 @@ import fr.diginamic.entites.Collegue;
 import fr.diginamic.services.CollegueService;
 
 @RestController
-@RequestMapping(path = "/tp")
 public class CollegueController {
 
 	@GetMapping("/collegues")
